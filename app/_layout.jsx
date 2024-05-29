bc2d3c760e5e06d90091e388b27fd1300ff50846
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-29 19:31:01 +0530

    chore: Update app.json with corrected scheme name and remove unnecessary newline in _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Slot } from 'expo-router'
 
 const RootLayout = () => {
-  return (
-    <View>
-      <Text>RootLayout</Text>
-    </View>
-  )
+  return <Slot />
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({})
