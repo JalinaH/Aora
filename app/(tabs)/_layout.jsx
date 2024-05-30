919f19b62471d08faa04f112528638395696b38b
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-30 18:09:36 +0530

    ```text feat: Add tab navigation to home screen
    The code changes include adding tab navigation to the home screen using the `expo-router` library. This allows users to navigate between different tabs in the application.

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { icons } from "../../constants";
+import { Tabs } from "expo-router";
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image 
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="h-6 w-6"
+       />
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
-  )
-}
+    <Tabs>
+      <Tabs.Screen 
+        name="home"
+        options={{
+          title: 'Home',
+          headerShown: 'false',
+          tabBarIcon: ({ color, focused}) => (
+            <TabIcon 
+              icon={icons.home}
+              color={color}
+              name="Home"
+              focused={focused}
+            />
+          )
+        }}
+      />
+    </Tabs>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
