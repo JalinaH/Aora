6425ef22c47b0112807fab5f338b1e26fa8d881f
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-31 01:13:01 +0530

    refactor: Update FormField component with dynamic styling and props
    The code changes refactor the FormField component in the components/FormField.jsx file. The component now accepts additional props such as title, value, placeholder, handleChangeText, and otherStyles. It also applies dynamic styling based on the provided props. This improves the reusability and flexibility of the FormField component.

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-const FormField = () => {
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
   return (
-    <View>
-      <Text>FormField</Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default FormField
+export default FormField;
