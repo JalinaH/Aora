ccab9c5f501f65bb6ba0bee75c95e8421ee70d97
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-30 18:24:36 +0530

    refactor: Update tab layout component and add icons
    The code changes refactor the tab layout component in the _layout.jsx file and add icons for each tab. This improves the visual appearance and user experience of the tab navigation in the application.

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -5,33 +5,87 @@ import { Tabs } from "expo-router";
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
-    <View>
-      <Image 
+    <View className="items-center justify-center gap-2">
+      <Image
         source={icon}
         resizeMode="contain"
         tintColor={color}
         className="h-6 w-6"
-       />
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+      >
+        {name}
+      </Text>
     </View>
   );
 };
 
 const TabsLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen 
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
         name="home"
         options={{
-          title: 'Home',
-          headerShown: 'false',
-          tabBarIcon: ({ color, focused}) => (
-            <TabIcon 
+          title: "Home",
+          headerShown: "false",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
               icon={icons.home}
               color={color}
               name="Home"
               focused={focused}
             />
-          )
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookmark"
+        options={{
+          title: "Bookmark",
+          headerShown: "false",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.bookmark}
+              color={color}
+              name="Bookmark"
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          headerShown: "false",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.plus}
+              color={color}
+              name="Create"
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: "false",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.profile}
+              color={color}
+              name="Profile"
+              focused={focused}
+            />
+          ),
         }}
       />
     </Tabs>
