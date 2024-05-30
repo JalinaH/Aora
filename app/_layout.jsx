9055471467295e62e055308c0f71f4bc68206ec8
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-30 16:41:15 +0530

    Refactor root layout component, add new index file, and update app.json

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot } from 'expo-router'
+import { StyleSheet, Text, View } from "react-native";
+import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
-  return <Slot />
-}
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
