a6790199bb28b0fada7f9b8d4dc8f0f290719015
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-31 11:29:29 +0530

    refactor: Add CustomButton component and update sign-in form
    The code changes refactor the sign-in form in the app/(auth)/sign-in.jsx file. The FormField component is updated with dynamic styling and props, allowing for improved reusability and flexibility. Additionally, the CustomButton component is added to the form, providing a custom button with a handlePress prop and loading state. These changes enhance the functionality and user experience of the sign-in form.

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -4,12 +4,18 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -37,6 +43,20 @@ const SignIn = () => {
             handleChangeText={(e) => setForm({ ...FormField, password: e })}
             otherStyles="mt-7"
           />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link href="/sign-up" className="text-lg text-secondary font-psemibold">Sign Up</Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
