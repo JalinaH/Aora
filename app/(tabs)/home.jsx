46490055dc78c15f5c787f51dc07ecdad3b5fd35
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-06-01 09:10:58 +0530

    refactor: Update sign-in and sign-up forms with lazy loading for fonts, error handling, navigation, and global state management

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList } from "react-native-web";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <FlatList
+        data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.id}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
